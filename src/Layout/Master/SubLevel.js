14aab3cf3025ab542a74ec12854773dc165742c1
Naveen G <naveen.g@esds.co.in>
2020-10-09 03:51:41 -0700

    Templet for CG-pro Admin dashboard

---
+++ src/Layout/Master/SubLevel.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import { Row, Col, Card, Table } from 'react-bootstrap';
+
+import Aux from "../../hoc/_Aux";
+import DEMO from "../../store/constant";
+
+import avatar1 from '../../assets/images/user/avatar-1.jpg';
+import avatar2 from '../../assets/images/user/avatar-2.jpg';
+import avatar3 from '../../assets/images/user/avatar-3.jpg';
+
+class Sublevels extends React.Component {
+    render() {
+        return (
+            <Aux>
+                <Row>
+                    <Col>
+                        <Card className='Recent-Users'>
+                            <Card.Header>
+                                <Card.Title as='h5'>Sublevels</Card.Title>
+                            </Card.Header>
+                            <Card.Body>
+                                <Table responsive hover>
+                                    <thead>
+                                        <tr>
+                                            <th>SubLevel No</th>
+                                            <th>SubLevel Name</th>
+                                            <th>Level Name</th>
+                                            <th>Descriptions</th>
+                                            <th>Update/Delete</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <th scope="row">
+                                                1
+                                                <img className="rounded-circle" style={{ width: '40px' }} src={avatar1} alt="activity-user" />
+                                            </th>
+                                            <td>Mark</td>
+                                            <td>
+                                                <h6 className="mb-1">Isabella Christensen</h6>
+                                                <p className="m-0">Lorem Ipsum is simply dummy text of…</p>
+                                            </td>
+                                            <td>
+                                                <h6 className="text-muted"><i className="fa fa-circle text-c-red f-10 m-r-15" />11 MAY 12:56</h6>
+                                            </td>
+                                            <td><a href={DEMO.BLANK_LINK} className="label theme-bg2 text-white f-12">Delete</a><a href={DEMO.BLANK_LINK} className="label theme-bg text-white f-12">Update</a></td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">
+                                                2
+                                                <img className="rounded-circle" style={{ width: '40px' }} src={avatar2} alt="activity-user" />
+                                            </th>
+                                            <td>Mark</td>
+                                            <td>
+                                                <h6 className="mb-1">Isabella Christensen</h6>
+                                                <p className="m-0">Lorem Ipsum is simply dummy text of…</p>
+                                            </td>
+                                            <td>
+                                                <h6 className="text-muted"><i className="fa fa-circle text-c-green f-10 m-r-15" />11 MAY 12:56</h6>
+                                            </td>
+                                            <td><a href={DEMO.BLANK_LINK} className="label theme-bg2 text-white f-12">Delete</a><a href={DEMO.BLANK_LINK} className="label theme-bg text-white f-12">Update</a></td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">
+                                                3
+                                                <img className="rounded-circle" style={{ width: '40px' }} src={avatar3} alt="activity-user" />
+                                            </th>
+                                            <td>Mark</td>
+                                            <td>
+                                                <h6 className="mb-1">Isabella Christensen</h6>
+                                                <p className="m-0">Lorem Ipsum is simply dummy text of…</p>
+                                            </td>
+                                            <td>
+                                                <h6 className="text-muted"><i className="fa fa-circle text-c-green f-10 m-r-15" />11 MAY 12:56</h6>
+                                            </td>
+                                            <td><a href={DEMO.BLANK_LINK} className="label theme-bg2 text-white f-12">Delete</a><a href={DEMO.BLANK_LINK} className="label theme-bg text-white f-12">Update</a></td>
+                                        </tr>
+                                    </tbody>
+                                </Table>
+                            </Card.Body>
+                        </Card>
+
+                    </Col>
+                </Row>
+            </Aux>
+        );
+    }
+}
+
+export default Sublevels;
---
+++ src/routes.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import $ from 'jquery';
+
+window.jQuery = $;
+window.$ = $;
+global.jQuery = $;
+
+const DashboardDefault = React.lazy(() => import('./Demo/Dashboard/Default'));
+
+const UIBasicButton = React.lazy(() => import('./Demo/UIElements/Basic/Button'));
+const UIBasicBadges = React.lazy(() => import('./Demo/UIElements/Basic/Badges'));
+const UIBasicBreadcrumbPagination = React.lazy(() => import('./Demo/UIElements/Basic/BreadcrumbPagination'));
+
+const UIBasicCollapse = React.lazy(() => import('./Demo/UIElements/Basic/Collapse'));
+const UIBasicTabsPills = React.lazy(() => import('./Demo/UIElements/Basic/TabsPills'));
+const UIBasicBasicTypography = React.lazy(() => import('./Demo/UIElements/Basic/Typography'));
+
+const FormsElements = React.lazy(() => import('./Demo/Forms/FormsElements'));
+
+const BootstrapTable = React.lazy(() => import('./Demo/Tables/BootstrapTable'));
+
+const Nvd3Chart = React.lazy(() => import('./Demo/Charts/Nvd3Chart/index'));
+
+const GoogleMap = React.lazy(() => import('./Demo/Maps/GoogleMap/index'));
+
+const OtherSamplePage = React.lazy(() => import('./Demo/Other/SamplePage'));
+const OtherDocs = React.lazy(() => import('./Demo/Other/Docs'));
+const Users = React.lazy(() => import('./Layout/User/Users'));
+const Employee = React.lazy(() => import('./Layout/User/Employee'));
+const Levels = React.lazy(() => import('./Layout/Master/Levels'));
+const SubLevels = React.lazy(() => import('./Layout/Master/SubLevel'));
+const Questions = React.lazy(() => import('./Layout/Master/Question'));
+const Options = React.lazy(() => import('./Layout/Master/Options'));
+const Hint = React.lazy(() => import('./Layout/Master/Hint'));
+
+const routes = [
+    { path: '/Hint', exact: true, name: 'Bootstrap Table', component: Hint },
+    { path: '/Options', exact: true, name: 'Bootstrap Table', component: Options },
+    { path: '/Questions', exact: true, name: 'Bootstrap Table', component: Questions },
+    { path: '/SubLevels', exact: true, name: 'Bootstrap Table', component: SubLevels },
+    { path: '/Levels', exact: true, name: 'Bootstrap Table', component: Levels },
+    { path: '/Employee', exact: true, name: 'Bootstrap Table', component: Employee },
+    { path: '/Users', exact: true, name: 'Bootstrap Table', component: Users },
+    { path: '/dashboard', exact: true, name: 'Default', component: DashboardDefault },
+    { path: '/button', exact: true, name: 'Basic Button', component: UIBasicButton },
+    { path: '/badges', exact: true, name: 'Basic Badges', component: UIBasicBadges },
+    { path: '/breadcrumb-paging', exact: true, name: 'Basic Breadcrumb Pagination', component: UIBasicBreadcrumbPagination },
+    { path: '/collapse', exact: true, name: 'Basic Collapse', component: UIBasicCollapse },
+    { path: '/tabs-pills', exact: true, name: 'Basic Tabs & Pills', component: UIBasicTabsPills },
+    { path: '/typography', exact: true, name: 'Basic Typography', component: UIBasicBasicTypography },
+    { path: '/forms', exact: true, name: 'Forms Elements', component: FormsElements },
+    { path: '/tables', exact: true, name: 'Bootstrap Table', component: BootstrapTable },
+    { path: '/charts/nvd3', exact: true, name: 'Nvd3 Chart', component: Nvd3Chart },
+    { path: '/maps/google-map', exact: true, name: 'Google Map', component: GoogleMap },
+    { path: '/sample-page', exact: true, name: 'Sample Page', component: OtherSamplePage },
+    { path: '/docs', exact: true, name: 'Documentation', component: OtherDocs },
+];
+
+export default routes;
