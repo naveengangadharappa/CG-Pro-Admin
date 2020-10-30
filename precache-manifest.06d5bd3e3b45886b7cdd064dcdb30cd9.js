35bdf574f1644454fb52d5a3271159b353273d06
Naveen G <naveen.g@esds.co.in>
2020-10-30 14:28:19 +0530

    Updates

---
+++ precache-manifest.06d5bd3e3b45886b7cdd064dcdb30cd9.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "432d41637d7dde6ccb6c0db1df344a2b",
+    "url": "/CG-Pro-Admin/index.html"
+  },
+  {
+    "revision": "2404defccb07e58933d0",
+    "url": "/CG-Pro-Admin/static/css/1.ebab8131.chunk.css"
+  },
+  {
+    "revision": "bb49270fab009a4fa8f6",
+    "url": "/CG-Pro-Admin/static/css/4.5bc6b0ce.chunk.css"
+  },
+  {
+    "revision": "0d8248198cff705b4f50",
+    "url": "/CG-Pro-Admin/static/css/5.9e762d10.chunk.css"
+  },
+  {
+    "revision": "2590f094e5f33811838e",
+    "url": "/CG-Pro-Admin/static/css/7.79d0ff5f.chunk.css"
+  },
+  {
+    "revision": "1b48b02e298e6214ffa9",
+    "url": "/CG-Pro-Admin/static/js/0.d378c59d.chunk.js"
+  },
+  {
+    "revision": "2404defccb07e58933d0",
+    "url": "/CG-Pro-Admin/static/js/1.182c0d8d.chunk.js"
+  },
+  {
+    "revision": "0a304a7bda2529ad92bd",
+    "url": "/CG-Pro-Admin/static/js/10.ec55a6af.chunk.js"
+  },
+  {
+    "revision": "b8e5b439f368f7512269",
+    "url": "/CG-Pro-Admin/static/js/11.611fbb59.chunk.js"
+  },
+  {
+    "revision": "dcc4bd7c900e846846cc",
+    "url": "/CG-Pro-Admin/static/js/12.1892ca8c.chunk.js"
+  },
+  {
+    "revision": "76e492e674261dbe0b06",
+    "url": "/CG-Pro-Admin/static/js/13.a95c3e0d.chunk.js"
+  },
+  {
+    "revision": "e5e9fbf5151d0bf371b7",
+    "url": "/CG-Pro-Admin/static/js/14.a1c29632.chunk.js"
+  },
+  {
+    "revision": "f91e1603a37446fe9f89",
+    "url": "/CG-Pro-Admin/static/js/15.2df666a5.chunk.js"
+  },
+  {
+    "revision": "36ef3d41158f0749adbf",
+    "url": "/CG-Pro-Admin/static/js/16.cae653f6.chunk.js"
+  },
+  {
+    "revision": "d78fed3aabed072140ba",
+    "url": "/CG-Pro-Admin/static/js/17.d7778bcf.chunk.js"
+  },
+  {
+    "revision": "66e07b51abd62b7475f2",
+    "url": "/CG-Pro-Admin/static/js/18.546a7770.chunk.js"
+  },
+  {
+    "revision": "88ebb01ba12d43b2df81",
+    "url": "/CG-Pro-Admin/static/js/19.e8ebe688.chunk.js"
+  },
+  {
+    "revision": "f0833ef4184a45b98674",
+    "url": "/CG-Pro-Admin/static/js/20.374aac62.chunk.js"
+  },
+  {
+    "revision": "9098d7f158ce9bc83830",
+    "url": "/CG-Pro-Admin/static/js/21.058a22a8.chunk.js"
+  },
+  {
+    "revision": "2840f97dc4a7f9f6f890",
+    "url": "/CG-Pro-Admin/static/js/22.c3b80f56.chunk.js"
+  },
+  {
+    "revision": "f3c03bec3c818c1f3dab",
+    "url": "/CG-Pro-Admin/static/js/23.2cb29cf7.chunk.js"
+  },
+  {
+    "revision": "46b530619f332ea281ad",
+    "url": "/CG-Pro-Admin/static/js/24.c8b89bbc.chunk.js"
+  },
+  {
+    "revision": "7743200ac4fc28c1e2a5",
+    "url": "/CG-Pro-Admin/static/js/25.f1dff0f2.chunk.js"
+  },
+  {
+    "revision": "82c220b80943db89d90a",
+    "url": "/CG-Pro-Admin/static/js/26.14b5c3a9.chunk.js"
+  },
+  {
+    "revision": "f8c1b2e55e7a83462342",
+    "url": "/CG-Pro-Admin/static/js/27.da6d514d.chunk.js"
+  },
+  {
+    "revision": "cf5ea28f618cc4d38198",
+    "url": "/CG-Pro-Admin/static/js/28.7d60e39d.chunk.js"
+  },
+  {
+    "revision": "075cba5632dee9cb26c4",
+    "url": "/CG-Pro-Admin/static/js/29.adec7f89.chunk.js"
+  },
+  {
+    "revision": "6bbb200a3685fe19e1a8",
+    "url": "/CG-Pro-Admin/static/js/30.d39507fc.chunk.js"
+  },
+  {
+    "revision": "bc9effad0fab7ad6a915",
+    "url": "/CG-Pro-Admin/static/js/31.ab6aed34.chunk.js"
+  },
+  {
+    "revision": "b8d14d76223975777496",
+    "url": "/CG-Pro-Admin/static/js/32.fe2b1a9c.chunk.js"
+  },
+  {
+    "revision": "bb49270fab009a4fa8f6",
+    "url": "/CG-Pro-Admin/static/js/4.4a4ac1e9.chunk.js"
+  },
+  {
+    "revision": "0d8248198cff705b4f50",
+    "url": "/CG-Pro-Admin/static/js/5.c44928a8.chunk.js"
+  },
+  {
+    "revision": "1f31365ed973ef95a4da",
+    "url": "/CG-Pro-Admin/static/js/6.202709d2.chunk.js"
+  },
+  {
+    "revision": "2590f094e5f33811838e",
+    "url": "/CG-Pro-Admin/static/js/7.d1783ea1.chunk.js"
+  },
+  {
+    "revision": "b5718d057bc0829bd09b",
+    "url": "/CG-Pro-Admin/static/js/8.72d5385f.chunk.js"
+  },
+  {
+    "revision": "4a73def7e2ee2cfb997c",
+    "url": "/CG-Pro-Admin/static/js/9.5e567db7.chunk.js"
+  },
+  {
+    "revision": "4988fb87ee361e3b3c6c",
+    "url": "/CG-Pro-Admin/static/js/main.3b7baf45.chunk.js"
+  },
+  {
+    "revision": "20272c42a66830bc3328",
+    "url": "/CG-Pro-Admin/static/js/runtime~main.1a07d527.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/CG-Pro-Admin/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/CG-Pro-Admin/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/CG-Pro-Admin/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/CG-Pro-Admin/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/CG-Pro-Admin/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
